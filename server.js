1b677c106b2047974d940129df2ce1728ec4f5a5
Raghvendra <rannyshukla762@gmail.com>
2022-09-22 10:57:56 +0530

    third commit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express=require("express");
 const cors=require("cors");
-const port=3000;
+const port=process.env.PORT || 3000;
 const app=express();
 const {JEEdata}=require("./JEEdata");
 const {NEETdata}=require("./NEETdata");
